8c5040bbd0759ce9f4906b78b40d17d8f9c51357
vairojarela <vairojarela@gmail.com>
2019-11-07 16:02:13 +0100

    fixed deploy

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import countries from "../countries.json";
+import countries from "../countries.json.js";
 import { Link } from "react-router-dom";
 
 const SideNav = () => {
