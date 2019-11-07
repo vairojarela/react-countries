23af8a126b52f5249298823d9d71e190d9ec1437
vairojarela <vairojarela@gmail.com>
2019-11-07 16:32:29 +0100

    Updates

--- precache-manifest.326b604a6962962e6a27068b39c484d7.js
+++ precache-manifest.326b604a6962962e6a27068b39c484d7.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "97f95d0cef9668afda7d",
+    "revision": "e607a47cae6fb5bb597f",
     "url": "/react-countries/static/css/main.36d1ff1e.chunk.css"
   },
   {
-    "revision": "97f95d0cef9668afda7d",
-    "url": "/react-countries/static/js/main.97f95d0c.chunk.js"
+    "revision": "e607a47cae6fb5bb597f",
+    "url": "/react-countries/static/js/main.e607a47c.chunk.js"
   },
   {
-    "revision": "8695253bb95f557114f8",
+    "revision": "280c668b020161739d11",
     "url": "/react-countries/static/css/1.c61a68b0.chunk.css"
   },
   {
-    "revision": "8695253bb95f557114f8",
-    "url": "/react-countries/static/js/1.8695253b.chunk.js"
+    "revision": "280c668b020161739d11",
+    "url": "/react-countries/static/js/1.280c668b.chunk.js"
   },
   {
     "revision": "8fef0691a002133fbf49",
     "url": "/react-countries/static/js/runtime~main.8fef0691.js"
   },
   {
-    "revision": "40d3980da7ec441b711cf2db02169dbb",
+    "revision": "621b19a274e598137d46ba94e0cacbd8",
     "url": "/react-countries/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-countries/precache-manifest.fafaf2759f6897eb2439b22d51b80b49.js"
+  "/react-countries/precache-manifest.326b604a6962962e6a27068b39c484d7.js"
 );
 
 workbox.clientsClaim();
