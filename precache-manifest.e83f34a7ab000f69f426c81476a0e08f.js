87152ca404b76d7082326317a580964f428a67de
vairojarela <vairojarela@gmail.com>
2019-11-07 17:04:50 +0100

    Updates

---
+++ precache-manifest.e83f34a7ab000f69f426c81476a0e08f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d560dea4d8c3d49f9fb11d2bc6883cf3",
+    "url": "/react-countries/index.html"
+  },
+  {
+    "revision": "0ad1499601f43eb8e354",
+    "url": "/react-countries/static/css/2.9720967d.chunk.css"
+  },
+  {
+    "revision": "06b70fbbf3045ab84e4d",
+    "url": "/react-countries/static/css/main.68a98085.chunk.css"
+  },
+  {
+    "revision": "0ad1499601f43eb8e354",
+    "url": "/react-countries/static/js/2.81c1fc39.chunk.js"
+  },
+  {
+    "revision": "06b70fbbf3045ab84e4d",
+    "url": "/react-countries/static/js/main.f2b2e4d7.chunk.js"
+  },
+  {
+    "revision": "dc221e2109f90d56d66d",
+    "url": "/react-countries/static/js/runtime-main.2842d1d9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-countries/precache-manifest.915149a0c808c27e435a8d4a45c61fe2.js"
+  "/react-countries/precache-manifest.e83f34a7ab000f69f426c81476a0e08f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-countries/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-countries/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
